2879cebdae4e38c9949baf0e2ce609a2ca1423d9
Samantha Spensley <spenslsm@gmail.com>
2016-08-29 21:34:06 -0600

    working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../lib/data.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Listing All People',
+    data: data.all
+  });
 });
 
 module.exports = router;
